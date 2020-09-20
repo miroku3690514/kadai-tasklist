e6cbedc7f5d84b3d7fb7c2076f07a6f999b4d64b
miroku <magic.miroku369@gmail.com>
2020-09-20 06:32:52 +0000

    error message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,6 +25,7 @@
         </nav>
     </header>
     <div class="container">
+        @include('commons.error_tasks')
         @yield('content')
     </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
